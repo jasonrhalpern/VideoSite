b2c69949cac08ba6e38de6f0c69bc9691de74190
jasonrhalpern <jasonrhalpern@gmail.com>
2013-03-11 20:07:03 -0400

    adding config for aws

--- Classes/Database.php
+++ Classes/Database.php
@@ -7,27 +7,16 @@
 interface Database
 {
     public function connect();
-
     public function disconnect();
-
     public function getDBConnection();
-
     public function isConnected();
-
     public function dataExists($query);
-
     public function isExecuted($query);
-
     public function insertUser($user);
-
     public function deleteUser($user);
-
     public function userExists($user);
-
     public function insertSeries($series);
-
     public function deleteSeries($series);
-
     public function seriesExists($series);
 
 }
